bddc5799815b7f3f8d68e7b97be4e083f9edfdf1
VALERIY-MITIOGLO <87361814+VALERIY-MITIOGLO@users.noreply.github.com>
2023-11-04 21:08:50 +0300

    init commit

---
+++ lib/generated/intl/messages_ru.dart
@@ -0,0 +1,64 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ru locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ru';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "a_ml" : MessageLookupByLibrary.simpleMessage("A - движение влево"),
+    "about" : MessageLookupByLibrary.simpleMessage("О нас"),
+    "bimbim" : MessageLookupByLibrary.simpleMessage("Бимбим"),
+    "c" : MessageLookupByLibrary.simpleMessage("°С"),
+    "d_mr" : MessageLookupByLibrary.simpleMessage("D - движение вправо"),
+    "developed_text" : MessageLookupByLibrary.simpleMessage("версия v1.0. Разработано ИУ7, лучшей кафедрой в мире."),
+    "direction" : MessageLookupByLibrary.simpleMessage("Направление"),
+    "enter_password" : MessageLookupByLibrary.simpleMessage("Пожалуйста, введите пароль.."),
+    "enter_username" : MessageLookupByLibrary.simpleMessage("Пожалуйста, введите имя пользователя.."),
+    "env" : MessageLookupByLibrary.simpleMessage("Параметры среды"),
+    "f" : MessageLookupByLibrary.simpleMessage("°Ф"),
+    "home" : MessageLookupByLibrary.simpleMessage("Главная"),
+    "humidity" : MessageLookupByLibrary.simpleMessage("Влажность"),
+    "khlamid" : MessageLookupByLibrary.simpleMessage("Хламид"),
+    "kmh" : MessageLookupByLibrary.simpleMessage("км / ч"),
+    "language_text" : MessageLookupByLibrary.simpleMessage("Язык"),
+    "locale" : MessageLookupByLibrary.simpleMessage("Русский"),
+    "login" : MessageLookupByLibrary.simpleMessage("Войти"),
+    "logout" : MessageLookupByLibrary.simpleMessage("Выйти"),
+    "mmhg" : MessageLookupByLibrary.simpleMessage("мм рт.ст."),
+    "monad" : MessageLookupByLibrary.simpleMessage("Монад"),
+    "mphh" : MessageLookupByLibrary.simpleMessage("миль / ч"),
+    "ms" : MessageLookupByLibrary.simpleMessage("м / с"),
+    "navigation" : MessageLookupByLibrary.simpleMessage("Навигация"),
+    "p_autopilot" : MessageLookupByLibrary.simpleMessage("P - автопилот"),
+    "pa" : MessageLookupByLibrary.simpleMessage("Па"),
+    "password" : MessageLookupByLibrary.simpleMessage("Пароль"),
+    "power" : MessageLookupByLibrary.simpleMessage("Мощность"),
+    "pressure" : MessageLookupByLibrary.simpleMessage("Давление"),
+    "questions_text" : MessageLookupByLibrary.simpleMessage("Есть какие-либо вопросы? Напиши нам: iu7@bmstu.com"),
+    "s_mb" : MessageLookupByLibrary.simpleMessage("S - движение вниз"),
+    "settings" : MessageLookupByLibrary.simpleMessage("Настройки"),
+    "speed" : MessageLookupByLibrary.simpleMessage("Скорость"),
+    "submit" : MessageLookupByLibrary.simpleMessage("Подтвердить"),
+    "temperature" : MessageLookupByLibrary.simpleMessage("Температура"),
+    "units" : MessageLookupByLibrary.simpleMessage("Единицы измерения"),
+    "username" : MessageLookupByLibrary.simpleMessage("Имя пользователя"),
+    "w_mf" : MessageLookupByLibrary.simpleMessage("W - движение вверх"),
+    "wrong_password" : MessageLookupByLibrary.simpleMessage("Не корректный пароль..")
+  };
+}
